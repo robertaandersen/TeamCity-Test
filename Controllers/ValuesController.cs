b8a18fe548a59b7e15d75128ba9a66bb40a77103
Róbert Sturla Reynisson <>
2019-05-14 13:33:49 +0000

    Adding val3

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace TeamCity_Test.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "val3" };
         }
 
         // GET api/values/5
